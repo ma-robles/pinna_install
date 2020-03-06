008e1762c85ba662a151b69f533f5c1a34c5f589
miguel robles <miguel.robles@atmosfera.unam.mx>
2020-03-06 16:28:11 -0600

    error printig crontab fixed

--- install.sh
+++ install.sh
@@ -19,6 +19,6 @@ echo "*/30 * * * * cd /home/pi/p_code/; python3 new_update_ftp.py 132.248.8.31 p
 echo '* * * * * cd /home/pi/p_code/; sshpass -v -p"m4rR_cc405" scp minute.cca marr-cca@132.248.8.29:~/minute_'$id'.cca  > /home/pi/rt.log 2>&1' >> $cronfile
 echo "15  */6 * * * cd /home/pi/p_code/; git pull > /home/pi/git.log 2>&1" >> $cronfile
 echo '17 * * * * cd /home/pi/; sshpass -p"m4rR_cc405" scp *.log marr-cca@132.248.8.29:~/'$id >> $cronfile
-cat cronfile
+cat $cronfile
 crontab $cronfile
 rm $cronfile
