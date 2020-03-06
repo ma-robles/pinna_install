451a8a44bdd159f9217bf7d85c5fd891c803e653
miguel robles <miguel.robles@atmosfera.unam.mx>
2020-03-06 15:38:43 -0600

    bug on crontab

--- install.sh
+++ install.sh
@@ -13,9 +13,9 @@ rm $rclocal
 
 cronfile="aire_cron.bak"
 echo "# m h  dom mon dow   command" > $cronfile
-echo "*/30 * * * * cd /home/pi/p_code/; python3 new_update_ftp.py 132.248.8.31 pembu p3mbu "$1" > /home/pi/ftp.log 2>&1" >> $cronfile
+echo "*/30 * * * * cd /home/pi/p_code/; python3 new_update_ftp.py 132.248.8.31 pembu p3mbu " $1 " > /home/pi/ftp.log 2>&1" >> $cronfile
 echo '* * * * * cd /home/pi/p_code/; sshpass -v -p"m4rR_cc405" scp minute.cca marr-cca@132.248.8.29:~/minute_'$1'.cca  > /home/pi/rt.log 2>&1' >> $cronfile
 echo "15  */6 * * * cd /home/pi/p_code/; git pull > /home/pi/git.log 2>&1" >> $cronfile
-echo "17 * * * * cd /home/pi/; sshpass -p"m4rR_cc405" scp *.log marr-cca@132.248.8.29:~/"$1 >> $cronfile
+echo '17 * * * * cd /home/pi/; sshpass -p"m4rR_cc405" scp *.log marr-cca@132.248.8.29:~/'$1 >> $cronfile
 crontab $cronfile
 rm $cronfile
