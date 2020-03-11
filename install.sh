7eb58bcd132aa3f6fe9386e4c493f0b3b4ebd77c
miguel robles <miguel.robles@atmosfera.unam.mx>
2020-03-11 12:09:45 -0600

    auto install all

--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
 if [[ -n "$1" ]]; then
+        git clone https://github.com/ma-robles/p_code.git /home/pi/p_code
+        ssh 132.248.8.29
+        sudo apt install sshpass vim
         sudo ./update_localrc.sh $1
         ./load_cron.sh $1
 else
